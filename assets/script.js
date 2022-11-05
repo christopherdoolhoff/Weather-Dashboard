1457e05d3a00ad73347422054306e03b0abdcfd1
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2022-11-05 14:13:00 -0400

    added country drop down list to search

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,38 @@
 var APIKey = "ed3d004e5dc879ccfeedd30eab1f4cea";
+var baseURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}.";
+var city;
+
+var getUserRepos = function (user) {
+    var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+  
+    fetch(apiUrl)
+      .then(function (response) {
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function (data) {
+            console.log(data);
+            displayRepos(data, user);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to GitHub');
+      });
+  };
+  
+  var getFeaturedRepos = function (language) {
+    var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+  
+    fetch(apiUrl).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayRepos(data.items, language);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    });
+  };
+  
