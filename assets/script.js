543eda1847038619cd4b2dbe57662a83e7a24093
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2022-11-10 23:39:08 -0500

    updated README.md and added comments to the html and js files

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// declared variables
 var APIKey = "ed3d004e5dc879ccfeedd30eab1f4cea";
 var baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 var baseFiveURL = "https://api.openweathermap.org/data/2.5/forecast?"
@@ -7,13 +8,13 @@ var stateInput = document.querySelector('#state').value;
 var cityWeatherEl = document.querySelector('#city-weather');
 var fiveDayEl = document.querySelector('#five-day');
 var today = moment().format("ddd MMM Do, YYYY");
-
+// give date for todays weather
 $("#today").text(today);
 
     console.log(cityInput);
     console.log(countryInput);
     console.log(stateInput);
-
+// API call function with if statements to determine if country and state were used
 var getCityWeather = function () {
     if(countryInput == 0 && stateInput == 0){
     var apiUrl = baseURL + cityInput + '&units=imperial&appid=' + APIKey;
@@ -74,7 +75,7 @@ var getCityWeather = function () {
     });
     }
   };
-
+// API call function for the five day forecast
   var getFiveDayWeather = function (data) {
     var apiUrl = baseFiveURL + 'lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&cnt=5&units=imperial&appid=' + APIKey;
     console.log(apiUrl); 
@@ -94,7 +95,7 @@ var getCityWeather = function () {
           alert('Unable to connect');
         });
     }
-
+// event listener to determine if US is used to indicate if state should show as an option.
 $("#country").change(function(){
     cityInput = document.querySelector('#city').value;
     countryInput = document.querySelector('#country').value;
@@ -106,6 +107,7 @@ $("#country").change(function(){
         document.querySelector("#state").style.display = "none";
     }
 })
+// event listener for the search button
   $("#searchBtn").click(function(){
     cityInput = document.querySelector('#city').value;
     countryInput = document.querySelector('#country').value;
@@ -113,20 +115,21 @@ $("#country").change(function(){
     console.log(cityInput);
     console.log(countryInput);
     console.log(stateInput);
+    // save search to local storage
     localStorage.setItem(
         "searched-" + document.querySelector('#city').value,document.querySelector('#city').value
     )
     getCityWeather();
 }
 );
-
+// function to write the API information for current weather to the page.
 var weatherToday = function (data){
     console.log(data);
     document.querySelector("#city-weather-div").style.display = "block";
 
     cityWeatherEl.innerHTML = "City: " + data.name + "<br>Temp: " + data.main.temp + "<br>Temp High: " + data.main.temp_max + "<br>Temp Low: " + data.main.temp_min + "<br>Wind Speed: " + data.wind.speed + "<br>Humidity: "+ data.main.humidity + "<br>Description: " + data.weather[0].description + '<br><img src="http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png" alt="weather condition image">'
 }
-
+// function to write the API information for the forecast weather to the page.
 var weatherForecast = function (data){
     console.log(data);
     document.querySelector("#five-day-div").style.display = "block";
@@ -139,7 +142,7 @@ var weatherForecast = function (data){
     fiveDayEl.appendChild(repoEl);
     }
 };
-
+// call local storage and add it to the search history
 for (var i = 0; i < localStorage.length; i++) {
     var key = localStorage.key(i);
     var value = localStorage.getItem(key);
@@ -150,7 +153,7 @@ for (var i = 0; i < localStorage.length; i++) {
     document.querySelector('#pastsearch').append(searchHistoryEl);
     searchHistoryEl.classList = 'list-group-item';
   };
-
+// event listener to call the search history for current weather and forecast
   $(".list-group-item").click(function (event) {
     cityInput = event.target.textContent
       console.log(event.target.textContent)
